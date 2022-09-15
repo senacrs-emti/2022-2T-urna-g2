87bf26cb8f30d64f8277bceb0db8673bb87e47d9
João Augusto Carbonera <joaocarbonera06@gmail.com>
2022-09-15 16:51:22 -0300

    front-mesario 15/09
    mesario front sim

---
+++ mesario.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mesario</title>
+    <link rel="stylesheet" href="mesario.css">
+</head>
+<body>
+    <main>
+        <section id="terminal">a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k</section>
+        <section id="teclado">
+            <div class="num">1</div>
+            <div class="num">2</div>
+            <div class="num">3</div>
+            <div class="num">4</div>
+            <div class="num">5</div>
+            <div class="num">6</div>
+            <div class="num">7</div>
+            <div class="num">8</div>
+            <div class="num">9</div>
+            <div class="num">0</div>
+        </section>
+    </main>
+</body>
+</html>
