48c077a598f81c28a43e3d76976d5287826700e0
joaoaugusto09 <joaocarbonera06@gmail.com>
2022-09-27 16:46:51 -0300

    27/09 Mini Att

--- mesario.php
+++ mesario.php
@@ -9,9 +9,10 @@
 </head>
 <body>
     <main>
-        <section id="terminal">a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k</section>
+        <input type="text" id="terminal">
         <section id="teclado">
-            <div class="num">1</div>
+            <div class="num">
+                1</div>
             <div class="num">2</div>
             <div class="num">3</div>
             <div class="num">4</div>
